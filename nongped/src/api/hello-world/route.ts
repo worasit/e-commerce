839d7d8f202281ed7d3f0c1e2418971c45cdfc39
em-daimongkol-tw <110962814+em-daimongkol-tw@users.noreply.github.com>
2024-12-06 15:41:36 +0700

    added a custom API route

---
+++ nongped/src/api/hello-world/route.ts
@@ -0,0 +1,7 @@
+import type {MedusaRequest, MedusaResponse} from '@medusajs/framework/http';
+
+export const GET = (req: MedusaRequest, res: MedusaResponse) => {
+    res.json({
+        message: 'Hello World!',
+    })
+}
